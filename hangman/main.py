43d2aee81b676834801a65d95319923efd43b5db
Chadaporn Patsuriya <6510110084@psu.ac.th>
2023-02-12 16:53:11 +0700

    add text underline

--- hangman/main.py
+++ hangman/main.py
@@ -1,10 +1,12 @@
 import kivy
 import string
+import random
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
+from words import WORDS
 
 class ButtonsLayout(GridLayout):
     INSTANCES = []
@@ -31,14 +33,18 @@ class MyRoot(BoxLayout):
     ERRORS = StringProperty()
     HANGMAN_IMG = StringProperty()
     GAME_MSG = StringProperty()
+    WORD_DISPLAY = StringProperty()
     def __init__(self, **kwargs):
         super(MyRoot, self).__init__(**kwargs)
+        self.RANDOM_WORD = ""
         self.button_layout = ButtonsLayout.INSTANCES[0]
         self.start_game()
     def start_game(self):
+        self.RANDOM_WORD = random.choice(WORDS)
         self.ERRORS = "0"
         self.HANGMAN_IMG = "images/hangman0.png"
         self.GAME_MSG = "Guess the word"
+        self.WORD_DISPLAY = " ".join(["_" for _ in self.RANDOM_WORD])
 
 
 class Hangman(App):
